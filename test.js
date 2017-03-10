4b22d7f6b89f7dbd9e77126569b87d6c8d58fe71
nickshevr <nickshevr@google.com>
2017-03-10 13:19:42 +0300

    Added random month

--- test.js
+++ test.js
@@ -17,16 +17,21 @@ driver.findElement(By.id('LastName')).sendKeys(userObect.lastName);
 driver.findElement(By.id('GmailAddress')).sendKeys(userObect.email.split('@')[0]);
 driver.findElement(By.id('Passwd')).sendKeys(userObect.password);
 driver.findElement(By.id('PasswdAgain')).sendKeys(userObect.password);
-driver.findElement(By.xpath(`//*[@id="BirthMonth"]/div[1]`)).sendKeys('a');
+driver.findElement(By.xpath(`//*[@id="BirthMonth"]/div[1]`)).sendKeys(userObect.month);
 driver.findElement(By.id('BirthDay')).sendKeys(userObect.day);
 driver.findElement(By.id('BirthYear')).sendKeys(userObect.year);
-driver.findElement(By.xpath(`//*[@id="Gender"]/div[1]`)).sendKeys('m'); // f -female, m - male
+driver.findElement(By.xpath(`//*[@id="Gender"]/div[1]`)).sendKeys(userObect.gender.slice(0, 1)); // f -female, m - male
 driver.findElement(By.xpath('//*[@id="submitbutton"]')).click();
 driver.sleep(1000);
 driver.findElement(By.xpath('//*[@id="tos-scroll-button"]')).click();
 driver.sleep(1000);
 driver.findElement(By.xpath('//*[@id="iagreebutton"]')).click();
 driver.sleep(5000);
+
+driver.getTitle().then(function(title) {
+    console.log(title);
+});
+
 driver.quit();
 
 //<div class="goog-inline-block goog-flat-menu-button-caption" id=":0" role="option" aria-setsize="12" aria-posinset="1">January</div>
--- utils/generator.js
+++ utils/generator.js
@@ -1,6 +1,11 @@
 const nameGen = require('node-random-name');
 const crypto = require('crypto');
 const lorem = require('lorem-ipsum');
+const MonthFirstLetter = [65, 68, 74, 79, 78, 83];
+
+function getRandomElemFromArr(arr) {
+    return arr[Math.floor(Math.random()*arr.length)];
+}
 
 exports.createUserObj = function () {
     const gender = Math.random() > 0.4 ? 'female' : 'male';
@@ -11,7 +16,7 @@ exports.createUserObj = function () {
         firstName: name.split(' ')[0],
         lastName: name.split(' ')[1],
         password: crypto.createHmac('sha1', lorem({ count: 1, units: 'words'}) + Date.now().toString()).digest('hex').slice(0, 11),
-        month: Math.floor(Math.random()*9),
+        month: String.fromCharCode(getRandomElemFromArr(MonthFirstLetter)),
         email: name.split(' ').join(`${lorem({ count: 1, units: 'words'})}`),
         day: Math.floor(Math.random()*27 + 1),
         year: Math.floor(Math.random()*30) + 1970
